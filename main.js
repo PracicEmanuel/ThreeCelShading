d1dc0d9c0dec4c1f20b186b5ecd92177d74c7c6d
Emanuel Pracic <epracic@tvz.hr>
2024-04-05 20:49:06 +0200

    cleanup of main

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ import * as THREE from 'three';
 
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
-import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader'
-
 import {EffectComposer} from 'three/examples/jsm/postprocessing/EffectComposer'
 import {RenderPass} from 'three/examples/jsm/postprocessing/RenderPass'
 
@@ -20,38 +18,6 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-
-const params = {
-  split:          true,       // optional, default: true
-  uvSmooth:       false,      // optional, default: false
-  preserveEdges:  false,      // optional, default: false
-  flatOnly:       false,      // optional, default: false
-  maxTriangles:   Infinity,   // optional, default: Infinity
-};
-
-
-const solidify = (mesh) =>{
-  const Thickness = 0.05
-  const geometry = mesh.geometry
-  const mat = new THREE.ShaderMaterial({
-    vertexShader: /* glsl */ `
-    void main(){
-      vec3 NewPosition = position + normal * ${Thickness};
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(NewPosition, 1);
-    }
-    `,
-    fragmentShader: /* glsl */ `
-    void main(){
-      gl_FragColor = vec4(1.0,1.0,1.0,1.0);
-    }
-    `,
-    side: THREE.BackSide
-
-  })
-
-  const outline = new THREE.Mesh(geometry, mat)
-  scene.add(outline)
-}
 
 
 const renderer = new THREE.WebGLRenderer({
@@ -77,48 +43,6 @@ const PointHelper = new THREE.PointLightHelper(PointLight);
 scene.add(PointHelper)
 
 
-
-const loader = new OBJLoader();
-
-const mat = new THREE.ShaderMaterial({
-  uniforms: {
-    textureMap: { value: texture }
-  },
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader
-})
-
-const CelShader = {
-  uniforms: {
-      modelColor : {value: new THREE.Color(0xAAFF00)},
-      lighting: {value: new THREE.Color(0xFFFFFF)},
-      ambient: {value: new THREE.Vector3(0.5,0.5,0.5)},
-      lightSourcePosition: {value: PointLight.position},
-      toneMap: texture
-  },
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader
-};
-
-// load a resource
-new OBJLoader( ).load( './models/Teapot.obj', function ( object ) {
-
-    // Now we find each Mesh...
-    object.traverse( function ( child ) {
-        if ( child instanceof THREE.Mesh ) {
-
-            // const geometry = LoopSubdivision.modify(new THREE.BoxGeometry(), iterations, params);
-            //let geometry = child.geometry;
-            //geometry = LoopSubdivision.modify(geometry, 2, params);
-            //child.geometry = geometry
-            child.material = new THREE.ShaderMaterial(CelShader)
-
-        }
-    });
-    solidify(object);
-    //scene.add( object );
-});
-
 const sphereMesh = new THREE.Mesh(new THREE.TorusKnotGeometry(1, 0.4, 128, 128, 2, 3), 
 new THREE.ShaderMaterial({
   uniforms: {
@@ -126,10 +50,9 @@ new THREE.ShaderMaterial({
     lighting: {value: new THREE.Color(0xFFFFFF)},
     ambient: {value: new THREE.Vector3(0.0,1.0,0.0)},
     lightSourcePosition: {value: PointLight.position}
-},
+  },
   vertexShader: vertexShader,
-  fragmentShader: fragmentShader,
-  side: THREE.DoubleSide
+  fragmentShader: fragmentShader
 }))
 
 
